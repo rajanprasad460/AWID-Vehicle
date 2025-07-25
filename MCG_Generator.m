e0d6e207b24c56b00d7fd35d4063528322b45086
Rajan Prasad <KU500934@kunet.ae>
2025-07-25 12:10:16 +0400

    Modelling approach now generalized for n-wheel vehicle

--- MCG_Generator.m
+++ MCG_Generator.m
@@ -2,182 +2,141 @@ clc;
 clear;
 close all;
 
-% === Define symbols ===
-syms x y z phi theta psi real   % COM position and orientation
+% === Parameters ===
+n = 6;  % number of wheels
+
+% === Define symbolic variables ===
+syms x y z phi theta psi real
 syms xd yd zd phid thetad psid real
 syms xdd ydd zdd phidd thetadd psidd real
-syms th1 th2 th3 th4 th5 th6 real
-syms th1d th2d th3d th4d th5d th6d real
-syms th1dd th2dd th3dd th4dd th5dd th6dd real
+
+q = [x; y; z; phi; theta; psi];
+qd = [xd; yd; zd; phid; thetad; psid];
+qdd = [xdd; ydd; zdd; phidd; thetadd; psidd];
+
+% Wheel symbolic variables
 syms mb mw Ibx Iby Ibz Iw g R l w real
-syms tau1 tau2 tau3 tau4 tau5 tau6 real
+params = [mb; mw; Ibx; Iby; Ibz; Iw; g; R; l; w];
 
-% Generalized coordinates and derivatives
-q = [x; y; z; phi; theta; psi; th1; th2; th3; th4; th5; th6];
-qd = [xd; yd; zd; phid; thetad; psid; th1d; th2d; th3d; th4d; th5d; th6d];
-qdd = [xdd; ydd; zdd; phidd; thetadd; psidd; th1dd; th2dd; th3dd; th4dd; th5dd; th6dd];
+q(7:6+n) = 0;
+qd(7:6+n) = 0;
+qdd(7:6+n) = 0;
+
+for i = 1:n
+    syms(['th', num2str(i)], 'real');
+    syms(['th', num2str(i), 'd'], 'real');
+    syms(['th', num2str(i), 'dd'], 'real');
+    syms(['tau', num2str(i)], 'real');
+
+    q(6+i) = eval(['th', num2str(i)]);
+    qd(6+i) = eval(['th', num2str(i), 'd']);
+    qdd(6+i) = eval(['th', num2str(i), 'dd']);
+end
+
 nq = length(q);
-
-vars_q  = q;
+vars_q = q;
 vars_qd = qd;
-params  = [mb; mw; Ibx; Iby; Ibz; Iw; g; R; l; w];
 
-% Transformation from Euler angle rates to body angular velocity
+% === Rotation Matrix & Angular Velocity ===
 T_euler = [1, 0, -sin(theta);
            0, cos(phi), sin(phi)*cos(theta);
            0, -sin(phi), cos(phi)*cos(theta)];
-omega_body = simplify(T_euler * [phid; thetad; psid]);
-
-% Inertia matrix of the car body
+omega_body = simplify(T_euler * qd(4:6));
 Ib = diag([Ibx, Iby, Ibz]);
 
-% Kinetic energy of the body
-T_body = (1/2)*mb*(xd^2 + yd^2 + zd^2) + (1/2)*(omega_body.' * Ib * omega_body);
+% === Kinetic Energy ===
+T_body = (1/2)*mb*sum(qd(1:3).^2) + (1/2)*omega_body.' * Ib * omega_body;
 
-% Wheel hub positions
-r_FL = [ l;  w; 0];
-r_FR = [ l; -w; 0];
-r_ML = [ 0;  w; 0];
-r_MR = [ 0; -w; 0];
-r_RL = [-l;  w; 0];
-r_RR = [-l; -w; 0];
+% === Wheel hub positions ===
+% Format: [x; y; z] in body frame (predefine for n=6, adjust as needed)
+hub_coords = {
+    [ l;  w; 0];
+    [ l; -w; 0];
+    [ 0;  w; 0];
+    [ 0; -w; 0];
+    [-l;  w; 0];
+    [-l; -w; 0];
+};
 
-r_FL_contact_b = r_FL + [0; 0; -R];
-r_FR_contact_b = r_FR + [0; 0; -R];
-r_ML_contact_b = r_ML + [0; 0; -R];
-r_MR_contact_b = r_MR + [0; 0; -R];
-r_RL_contact_b = r_RL + [0; 0; -R];
-r_RR_contact_b = r_RR + [0; 0; -R];
-
-p_body = [x; y; z];
+% Transformation
+p_body = q(1:3);
 R_body = combinedRotationMatrix(phi, theta, psi);
 
-r_FL_contact_w = R_body * r_FL_contact_b + p_body;
-r_FR_contact_w = R_body * r_FR_contact_b + p_body;
-r_ML_contact_w = R_body * r_ML_contact_b + p_body;
-r_MR_contact_w = R_body * r_MR_contact_b + p_body;
-r_RL_contact_w = R_body * r_RL_contact_b + p_body;
-r_RR_contact_w = R_body * r_RR_contact_b + p_body;
 
-J_FL = jacobian(r_FL_contact_w, q);
-J_FR = jacobian(r_FR_contact_w, q);
-J_ML = jacobian(r_ML_contact_w, q);
-J_MR = jacobian(r_MR_contact_w, q);
-J_RL = jacobian(r_RL_contact_w, q);
-J_RR = jacobian(r_RR_contact_w, q);
+tau = sym(zeros(n,1));
+T_wheel_trans = 0;
+T_wheel_rot = 0;
+z_all = sym(zeros(n,1));
 
-% Wheel angle selector vectors
-e_theta1 = zeros(1,nq); e_theta1(7) = 1;
-e_theta2 = zeros(1,nq); e_theta2(8) = 1;
-e_theta3 = zeros(1,nq); e_theta3(9) = 1;
-e_theta4 = zeros(1,nq); e_theta4(10) = 1;
-e_theta5 = zeros(1,nq); e_theta5(11) = 1;
-e_theta6 = zeros(1,nq); e_theta6(12) = 1;
 
-e_rolling_body = [1; 0; 0];
-e_vertical_body = [0; 0; 1];
+e_rolling = R_body * [1; 0; 0];
+e_lateral = R_body * [0; 1; 0];
+e_vertical = R_body * [0; 0; 1];
+% Filter by index if needed (e.g., only rolling, or all 3 constraints)
+key_idx = [1 3];
 
-e_rolling = R_body * e_rolling_body;
-e_vertical = R_body * e_vertical_body;
+% Jaocbians
 
-Jc_FL = [ e_rolling.' * J_FL - R * e_theta1; e_vertical.' * J_FL ];
-Jc_FR = [ e_rolling.' * J_FR - R * e_theta2; e_vertical.' * J_FR ];
-Jc_ML = [ e_rolling.' * J_ML - R * e_theta5; e_vertical.' * J_ML ];
-Jc_MR = [ e_rolling.' * J_MR - R * e_theta6; e_vertical.' * J_MR ];
-Jc_RL = [ e_rolling.' * J_RL - R * e_theta3; e_vertical.' * J_RL ];
-Jc_RR = [ e_rolling.' * J_RR - R * e_theta4; e_vertical.' * J_RR ];
+Jc_all = sym(zeros(length(key_idx),nq));
+Jdot_c_all = sym(zeros(length(key_idx),nq));
 
-Jc = [Jc_FL; Jc_FR; Jc_ML; Jc_MR; Jc_RL; Jc_RR];
 
-%% === Compute time derivatives Jdot_c ===
-Jdot_c_FL = sym(zeros(size(Jc_FL)));
-Jdot_c_FR = sym(zeros(size(Jc_FR)));
-Jdot_c_ML = sym(zeros(size(Jc_ML)));
-Jdot_c_MR = sym(zeros(size(Jc_MR)));
-Jdot_c_RL = sym(zeros(size(Jc_RL)));
-Jdot_c_RR = sym(zeros(size(Jc_RR)));
+for i = 1:n
+    r_i = hub_coords{i};
+    r_contact_b = r_i + [0; 0; -R];
+    r_contact_w = R_body * r_contact_b + p_body;
+    
+    J_i = jacobian(r_contact_w, q);
+    
+    e_theta = zeros(1,nq);
+    e_theta(6+i) = 1;
+    
 
-[m,n] = size(Jc_FL);
+    Jc_i = [e_rolling.' * J_i - R * e_theta;
+            e_lateral.' * J_i;
+            e_vertical.' * J_i];
+    
 
-for row = 1:m
-    for i = 1:n
-        dJ_dq1 = diff(Jc_FL(row,:), q(i));
-        Jdot_c_FL(row,:) = Jdot_c_FL(row,:) + dJ_dq1 * qd(i);
+    Jc_i = Jc_i(key_idx,:);
 
-        dJ_dq2 = diff(Jc_FR(row,:), q(i));
-        Jdot_c_FR(row,:) = Jdot_c_FR(row,:) + dJ_dq2 * qd(i);
-
-        dJ_dq3 = diff(Jc_ML(row,:), q(i));
-        Jdot_c_ML(row,:) = Jdot_c_ML(row,:) + dJ_dq3 * qd(i);
-
-        dJ_dq4 = diff(Jc_MR(row,:), q(i));
-        Jdot_c_MR(row,:) = Jdot_c_MR(row,:) + dJ_dq4 * qd(i);
-
-        dJ_dq5 = diff(Jc_RL(row,:), q(i));
-        Jdot_c_RL(row,:) = Jdot_c_RL(row,:) + dJ_dq5 * qd(i);
-
-        dJ_dq6 = diff(Jc_RR(row,:), q(i));
-        Jdot_c_RR(row,:) = Jdot_c_RR(row,:) + dJ_dq6 * qd(i);
+    % Stack the Jacobians
+    Jc_all((i-1)*length(key_idx)+1:(i*length(key_idx)),:) = Jc_i;
+    
+    % Compute Jdot
+    mJ = size(Jc_i,1);
+    Jdot_i = sym(zeros(mJ,nq));
+    for row = 1:mJ
+        for j = 1:nq
+            dJ = diff(Jc_i(row,:), q(j));
+            Jdot_i(row,:) = Jdot_i(row,:) + dJ * qd(j);
+        end
     end
+    % Jdot_c_all = [Jdot_c_all; Jdot_i];
+    % Stack the Jacobians dots
+    Jdot_c_all((i-1)*length(key_idx)+1:(i*length(key_idx)),:) = Jdot_i;
+
+    % Velocities
+    v_com = qd(1:3);
+    v_i = v_com + cross(omega_body, r_i);
+    T_wheel_trans = T_wheel_trans + (1/2)*mw*(v_i.'*v_i);
+    
+    thd = qd(6+i);
+    T_wheel_rot = T_wheel_rot + (1/2)*Iw*thd^2;
+    
+    z_i = p_body + R_body * r_i;
+    z_all(i) =  z_i(3);
+    
+    tau(i) = eval(['tau', num2str(i)]);
 end
 
-
-
-Jdot_c = [Jdot_c_FL;
-            Jdot_c_FR;
-            Jdot_c_ML;
-            Jdot_c_MR;
-            Jdot_c_RL;
-            Jdot_c_RR];   % 12 x 10
-%% === Export MATLAB functions ===
-
-
-matlabFunction(Jc, Jdot_c, ...
-    'Vars', {vars_q, vars_qd, params}, ...
-    'File', 'AllLegs_contactRolling_J_and_Jdot', ...
-    'Outputs', {'Jc', 'Jdot_c'});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-v_com = [xd; yd; zd];
-v_FL = v_com + cross(omega_body, r_FL);
-v_FR = v_com + cross(omega_body, r_FR);
-v_ML = v_com + cross(omega_body, r_ML);
-v_MR = v_com + cross(omega_body, r_MR);
-v_RL = v_com + cross(omega_body, r_RL);
-v_RR = v_com + cross(omega_body, r_RR);
-
-T_wheel_trans = (1/2)*mw*(v_FL.'*v_FL + v_FR.'*v_FR + v_ML.'*v_ML + v_MR.'*v_MR + v_RL.'*v_RL + v_RR.'*v_RR);
-T_wheel_rot   = (1/2)*Iw*(th1d^2 + th2d^2 + th3d^2 + th4d^2 + th5d^2 + th6d^2);
-
+% === Energies and Lagrangian ===
 T_total = simplify(T_body + T_wheel_trans + T_wheel_rot);
-
-z_FL = p_body + (R_body * r_FL);
-z_FR = p_body + (R_body * r_FR);
-z_ML = p_body + (R_body * r_ML);
-z_MR = p_body + (R_body * r_MR);
-z_RL = p_body + (R_body * r_RL);
-z_RR = p_body + (R_body * r_RR);
-
-V_total = mb * g * z + mw * g * (z_FL(3) + z_FR(3) + z_ML(3) + z_MR(3) + z_RL(3) + z_RR(3));
-
+V_total = mb*g*q(3) + mw*g*sum(z_all);
 L = T_total - V_total;
-Tau = [zeros(6,1); tau1; tau2; tau3; tau4; tau5; tau6];
 
+Tau_vec = [zeros(6,1); tau];
+
+% === Euler-Lagrange Equations ===
 EL_eqns = sym(zeros(nq,1));
 for i = 1:nq
     dL_dqdot = diff(L, qd(i));
@@ -189,6 +148,7 @@ for i = 1:nq
     EL_eqns(i) = simplify(ddt_dL_dqdot - dL_dq);
 end
 
+% === Mass (M), Coriolis (C), Gravity (G) ===
 M = sym(zeros(nq));
 for i = 1:nq
     for j = 1:nq
@@ -203,12 +163,12 @@ end
 
 C = simplify(EL_eqns - M*qdd - G);
 
-%%
-% EXporting the functions
-
+% === Export Functions ===
 matlabFunction(M, 'Vars', {vars_q, params}, 'File', 'M_matrix_func');
 matlabFunction(C, 'Vars', {vars_q, vars_qd, params}, 'File', 'C_vector_func');
 matlabFunction(G, 'Vars', {vars_q, params}, 'File', 'G_vector_func');
 
-
-
+matlabFunction(Jc_all, Jdot_c_all, ...
+    'Vars', {vars_q, vars_qd, params}, ...
+    'File', 'AllLegs_contactRolling_J_and_Jdot', ...
+    'Outputs', {'Jc', 'Jdot_c'});
